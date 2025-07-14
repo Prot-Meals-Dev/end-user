830c57dd173d17c277c7bac377f45b8aab1457ce
cuddleminddev <cuddleminddev@gmail.com>
2025-07-14 11:37:57 +0530

    initilisation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { AboutComponent } from './components/about/about.component';
+import { OrdersComponent } from './components/orders/orders.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { LoginComponent } from './components/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'home', component: HomeComponent },
+    { path: 'menu', component: MenuComponent },
+    { path: 'about', component: AboutComponent },
+    { path: 'orders', component: OrdersComponent },
+    { path: 'profile', component: ProfileComponent },
+    { path: 'login', component: LoginComponent }
+];
