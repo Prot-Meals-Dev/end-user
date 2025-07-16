b95be89a12092c9c1c167c0a2576f630f10ed3bc
cuddleminddev <cuddleminddev@gmail.com>
2025-07-16 11:02:37 +0530

    datepicker update

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,9 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { NgbCalendar, NgbDate, NgbDatepickerModule, NgbDateStruct, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-menu',
-  imports: [CommonModule],
+  imports: [CommonModule, NgbDatepickerModule],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.css'
 })
@@ -59,4 +60,20 @@ export class MenuComponent {
       dinner: 'Puttu, Kadala Curry'
     },
   ]
+
+  minDate: NgbDateStruct;
+  today: NgbDateStruct;
+
+  constructor(
+    private calendar: NgbCalendar,
+  ) {
+    this.today = this.calendar.getToday();
+    this.minDate = this.today;
+  }
+
+  isDateDisabled = (date: NgbDate, current?: { year: number; month: number }): boolean => {
+    const todayDate = new Date(this.today.year, this.today.month - 1, this.today.day);
+    const checkDate = new Date(date.year, date.month - 1, date.day);
+    return checkDate <= todayDate;
+  };
 }
