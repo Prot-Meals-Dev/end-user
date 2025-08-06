8ee060009ab5f728ac56b04d6b9d809db169a525
cuddleminddev <cuddleminddev@gmail.com>
2025-08-06 15:46:50 +0530

    order summary

--- src/app/components/order-summary/order-summary.component.ts
+++ src/app/components/order-summary/order-summary.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { SummaryService } from './service/summary.service';
 
 @Component({
   selector: 'app-order-summary',
@@ -11,22 +12,80 @@ export class OrderSummaryComponent implements OnInit {
 
   formData: any;
   totalAmount: number = 0;
+  user: any;
+  userDetails!: any;
+
+  mealTypes: string[] = [];
+  selectedDays: string[] = [];
+  formattedStartDate: string = '';
+  formattedEndDate: string = '';
 
   constructor(
-    private router: Router
+    private router: Router,
+    private service: SummaryService
   ) {
     const navigation = this.router.getCurrentNavigation();
     const state = navigation?.extras?.state as {
       formData: any;
       totalAmount: number;
+      user: any;
     };
 
     this.formData = state?.formData;
     this.totalAmount = state?.totalAmount;
+    this.user = state?.user;
   }
 
   ngOnInit(): void {
-    console.log(this.formData, this.totalAmount);
+    console.log(this.formData, this.totalAmount, this.user);
+
+    this.extractMealTypes();
+    this.extractRecurringDays();
+    this.formatDates();
+
+    this.getUserDetails()
+  }
+
+  getUserDetails() {
+    this.service.getUser(this.user.id).subscribe({
+      next: (res: any) => {
+        console.log(res);
+        this.userDetails = res.data;
+      },
+      error: (err: any) => {
+        console.log(err);
+      }
+    })
+  }
+
+  extractMealTypes() {
+    const types = this.formData;
+    this.mealTypes = [];
+    if (types?.breakfast) this.mealTypes.push('Breakfast');
+    if (types?.lunch) this.mealTypes.push('Lunch');
+    if (types?.dinner) this.mealTypes.push('Dinner');
+  }
+
+  extractRecurringDays() {
+    this.selectedDays = Object.entries(this.formData?.recurringDays || {})
+      .filter(([_, isSelected]) => isSelected)
+      .map(([day]) => day);
+  }
+
+  formatDates() {
+    const start = this.formData?.startDate;
+    const end = this.formData?.endDate;
+    if (start) this.formattedStartDate = `${start.day}/${start.month}/${start.year}`;
+    if (end) this.formattedEndDate = `${end.day}/${end.month}/${end.year}`;
+  }
+
+  changeAddress() {
+    console.log('Change Delivery Address clicked');
+  }
+
+  submitOrder() {
+    console.log('Submit clicked');
+    // Implement submit logic
   }
 
 }
---
+++ src/app/components/order-summary/service/summary.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../../../../environment/environment';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SummaryService {
+  private UserUrl = `${environment.apiUrl}/users`
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getUser(id: string) {
+    return this.http.get(`${this.UserUrl}/${id}`)
+  }
+}
