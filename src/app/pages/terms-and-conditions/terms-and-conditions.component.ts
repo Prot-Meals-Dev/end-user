5941d98a22a2f484051aeb169ef5222576783472
cuddleminddev <cuddleminddev@gmail.com>
2025-08-08 10:58:19 +0530

    contact form

--- src/app/pages/terms-and-conditions/terms-and-conditions.component.ts
+++ src/app/pages/terms-and-conditions/terms-and-conditions.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-terms-and-conditions',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './terms-and-conditions.component.html',
   styleUrl: './terms-and-conditions.component.css'
 })
